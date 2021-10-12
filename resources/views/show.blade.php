2b500c147fc6e0aa9b4a7c859825d82cd6807f87
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-10-12 12:59:07 -0300

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,18 +4,26 @@
 
 @section('content'){{--Extenção do arquivo template--}}
 
-
 <div id="dados-user">
-    @foreach ($reposArray as $repos)
-    <h1>Projetos Api GitHub</h1>
 
-    <li active>Usuário: {{ $repos['full_name'] }}</li>
+    <h1 class="dados">Projetos GitHub</h1>
+
+    @foreach ($reposArray as $repos)
 
     <ul>
+        <li class="dados" active>Usuário/projeto: {{ $repos['full_name'] }}</li>
+        <li class="dados" active>Acesso: {{ $repos['visibility'] }}</li>
+        <li class="dados">forks: {{ $repos['forks'] }}</li>
+        <li class="dados">default_branch: {{ $repos['default_branch'] }}</li>
+        <li class="dados">Data da criação: {{ $repos['created_at'] }}</li>
+        <br><a href="{{ $repos['html_url'] }}" class="links">Acessar repositório</a>
     </ul>
+    
+    <hr class="linha"></hr>
 
     @endforeach
 </div>
 
 @endsection {{--Extenção do arquivo template--}}
+
 
