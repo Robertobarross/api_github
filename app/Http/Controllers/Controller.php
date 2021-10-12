45eb2bc4596e06f0a7474c537dda6766c4134915
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-10-12 12:58:43 -0300

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Http;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function show(){ // Pesquisar repositórios
+        $repos = Http::all();
+
+        $search = request('search');
+        if($search){
+            $repos = Http::where([
+                ['repos', 'like', '%' .$search. '%']
+            ])->get();
+
+        } else {
+            $repos = Http::all(); // Pegando arquivos do Link
+        }
+        return view('show', ['repos' => $repos, 'search' => $search]);
+    }
 }
